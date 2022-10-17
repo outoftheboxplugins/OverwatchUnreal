f371363c09d80568dc6720f586c15fc9eb7df93e
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2022-10-17 20:06:40 +0300

    Added default code and config

---
+++ Source/OverwatchUnreal/OverwatchUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OverwatchUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVERWATCHUNREAL_API AOverwatchUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
